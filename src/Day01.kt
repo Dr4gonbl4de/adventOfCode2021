4f982f41dae1bc94fd2cd6b75873993ddab0c24e
Pascal Maczey <Pascalmaczey@web.de>
2021-12-09 18:26:09 +0100

    completed day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,32 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var last: Int? = null
+        val count = input.fold(0) { c, current ->
+            if (last != null && last!! < current.toInt()) {
+                last = current.toInt()
+                c + 1
+            } else {
+                last = current.toInt()
+                c
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lines = input.map { it.toInt() }
+        var last: Int? = null
+        return lines.foldIndexed(0) { index, acc, s ->
+            if (index + 2 >= lines.size) return acc
+            val newValue = s + lines[index + 1] + lines[index + 2]
+            if (last != null && last!! < newValue) {
+                last = newValue
+                acc + 1
+            } else {
+                last = newValue
+                acc
+            }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
